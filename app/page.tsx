5bb4702a15a241d48bfb4e181ee214b7e36477b2
guilhercs <guilherme.carvalhos@outlook.com>
2026-01-11 11:59:52 -0300

    feat(page): add hero image desktop/mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -28,7 +29,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Imagem desktop - esconde no mobile, mostra no desktop - hidden do tailwind esconde todos os elementos (igual display: none). Ja o md:block sobrescreve o hidden e mostra os elementos com pixels maiores que 768px. */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          {/* Imagem mobile - mostra no mobile, esconde no desktop - semelhante ao exemplo acima. */}
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
