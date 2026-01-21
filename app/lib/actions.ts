2da27f2758a3c274dddf853254b52206acfd3f2c
guilhercs <guilherme.carvalhos@outlook.com>
2026-01-21 15:03:19 -0300

    fix(invoices): ajusta conflitos

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,23 +17,6 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-import { z } from "zod";
-import postgres from "postgres";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
-
-const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
-  date: z.string(),
-});
-
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
